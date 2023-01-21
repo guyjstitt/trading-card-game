41c861f5c7c84ffc48db6ef9ef26026e4fda993f
Guy J. Stitt <guyjstitt@Guys-MacBook-Pro.local>
2023-01-21 03:03:43 -0700

    added summoning mechanic

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,104 @@
-import type { Component } from 'solid-js';
-
-import logo from './logo.svg';
+import { Component, createEffect, createSignal } from 'solid-js';
+import { fieldConfig } from "./field.config"
 import styles from './App.module.css';
-import { Button } from "@suid/material";
 
 const App: Component = () => {
+  const [fieldState, setFieldState] = createSignal(fieldConfig, { equals: false });
+  const [summon, setSummon] = createSignal({ player: "", handZoneId: 0, fieldZoneId: 0 }, { equals: false });
+
+  function handleClick(id: number, data: string[] | undefined) {
+    console.log('data', data)
+    setSummon((current) => {
+      console.log('set summon', data);
+      const isHandZoneUpdate = data && data.includes('hand-zone');
+      const isFieldZoneUpdate = data && data.includes('field-zone');
+      const isPlayerOne = data && data.includes('player-one');
+
+      console.log({
+        isHandZoneUpdate, isFieldZoneUpdate, isPlayerOne
+      })
+
+      current.player = isPlayerOne ? 'player-one' : 'player-two';
+
+      // temp logic, if the active and clicked player zone do not match, reset state
+      if(current.player && !data?.includes(current.player)) {
+        return { player: "", handZoneId: 0, fieldZoneId: 0 };
+      }
+
+      // if the player has clicked on a field zone before clicking in the handzone, do nothing
+      if ((isFieldZoneUpdate && !current.handZoneId)) {
+        return current;
+      }
+
+      // if the player clicks in the hand zone and no field zone id exists, update the hand zone id
+      if (isHandZoneUpdate && !current.fieldZoneId) {
+        current.handZoneId = id;
+        
+        return current;
+      }
+
+      // if the player has clicked in the hand zone and clicked in the field zone, update the field zone id
+      if (isFieldZoneUpdate && current.handZoneId) {
+        current.fieldZoneId = id;
+
+        return current;
+      }
+
+      return current;
+    });
+  }
+
+  function handleSummon({player, handZoneId, fieldZoneId}) {
+    console.log('effect');
+    console.log({
+      player, handZoneId, fieldZoneId
+    });
+
+    if (!player || !handZoneId || !fieldZoneId) {
+      return;
+    }
+
+    setFieldState((current) => {
+      const fieldZoneIndex = current.findIndex((element) => element.id === fieldZoneId);
+      const handZoneIndex = current.findIndex((element) => element.id === handZoneId);
+      
+      current[fieldZoneIndex].classList.push('single-card');
+
+      const classIndex = current[handZoneIndex].classList.indexOf('single-card');
+
+      current[handZoneIndex].classList.splice(classIndex, 1);
+
+      return current;
+    });
+
+    // reset summon data
+    setSummon({ player: "", handZoneId: 0, fieldZoneId: 0 });
+  }
+
+  createEffect(() => handleSummon(summon()));
+
+  // temp draw phase
+  setFieldState((current) => {
+    return current.map((zone) => {
+      if (zone.data?.includes('hand-zone')) {
+        zone.classList.push('single-card');
+
+        return zone;
+      }
+
+      return zone;
+    });
+  })
+
   return (
     <div class={styles.App}>
-    <div class="wrapper">
-      <div class="one">One</div>
-      <div class="two">Two</div>
-      <div class="three">Three</div>
-      <div class="four">Four</div>
-      <div class="five">Five</div>
-      <div class="six">Six</div>
-      <div class="seven">Seven</div>
-      <div class="one">One</div>
-      <div class="two">Two</div>
-      <div class="three">Three</div>
-      <div class="four">Four</div>
-      <div class="five">Five</div>
-      <div class="six">Six</div>
-      <div class="seven">Seven</div>
-      <div class="one">One</div>
-      <div class="two">Two</div>
-      <div class="three">Three</div>
-      <div class="four">Four</div>
-      <div class="five">Five</div>
-      <div class="six">Six</div>
-      <div class="seven">Graveyard</div>
-      <div class="player-one-extra-deck">Extra Deck</div>
-      <div class="player-one-card-one single-card">Card One</div>
-      <div class="player-one-card-two single-card">Card Two</div>
-      <div class="player-one-card-three single-card">Card Three</div>
-      <div class="player-one-card-four single-card">Card Four</div>
-      <div class="player-one-card-five single-card">Card Five</div>
-      <div class="player-one-card-deck">Deck</div>
-    </div>
-      {/* <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-      <Button variant="contained">Hello world</Button> */}
+      <div class="wrapper">
+        {
+          fieldState().map((value, index) => {
+            return <div class={value.classList.join(" ")} onClick={() =>handleClick(value.id, value.data)}></div>;
+          })
+        }
+      </div>
     </div>
   );
 };
---
+++ src/field.config.ts
@@ -0,0 +1,134 @@
+export const fieldConfig = [
+    {
+        "id": 1,
+        "classList": ["player-two-deck"],
+    },
+    {
+        "id": 2,
+        "classList": ["player-two-hand-zone"],
+        "data": ["hand-zone", "player-two"]
+    },
+    {
+        "id": 3,
+        "classList": ["player-two-hand-zone"],
+        "data": ["hand-zone", "player-two"]
+    },
+    {
+        "id": 4,
+        "classList": ["player-two-hand-zone"],
+        "data": ["hand-zone", "player-two"]
+    },
+    {
+        "id": 5,
+        "classList": ["player-two-hand-zone"],
+        "data": ["hand-zone", "player-two"]
+    },
+    {
+        "id": 6,
+        "classList": ["player-two-hand-zone"],
+        "data": ["hand-zone", "player-two"]
+    },
+    {
+        "id": 7,
+        "classList": ["player-two-extra-deck"]
+    },
+    {
+        "id": 8,
+        "classList": ["player-two-graveyard"]
+    },
+    {
+        "id": 9,
+        "classList": ["player-two-field-zone"],
+        "data": ["field-zone", "player-two"]
+    },
+    {
+        "id": 10,
+        "classList": ["player-two-field-zone"],
+        "data": ["field-zone", "player-two"]
+    },
+    {
+        "id": 11,
+        "classList": ["player-two-field-zone"],
+        "data": ["field-zone", "player-two"]
+    },
+    {
+        "id": 12,
+        "classList": ["player-two-field-zone"],
+        "data": ["field-zone", "player-two"]
+    },
+    {
+        "id": 13,
+        "classList": ["player-two-field-zone"],
+        "data": ["field-zone", "player-two"]
+    },
+    {
+        "id": 14,
+        "classList": ["player-two-empty"]
+    },
+    {
+        "id": 15,
+        "classList": ["player-one-empty"]
+    },
+    {
+        "id": 16,
+        "classList": ["player-one-field-zone"],
+        "data": ["field-zone", "player-one"]
+    },
+    {
+        "id": 17,
+        "classList": ["player-one-field-zone"],
+        "data": ["field-zone", "player-one"]
+    },
+    {
+        "id": 18,
+        "classList": ["player-one-field-zone"],
+        "data": ["field-zone", "player-one"]
+    },
+    {
+        "id": 19,
+        "classList": ["player-one-field-zone"],
+        "data": ["field-zone", "player-one"]
+    },
+    {
+        "id": 20,
+        "classList": ["player-one-field-zone"],
+        "data": ["field-zone", "player-one"]
+    },
+    {
+        "id": 21,
+        "classList": ["player-one-graveyard"]
+    },
+    {
+        "id": 22,
+        "classList": ["player-one-extra-deck"]
+    },
+    {
+        "id": 23,
+        "classList": ["player-one-hand-zone"],
+        "data": ["hand-zone", "player-one"]
+    },
+    {
+        "id": 24,
+        "classList": ["player-one-hand-zone"],
+        "data": ["hand-zone", "player-one"]
+    },
+    {
+        "id": 25,
+        "classList": ["player-one-hand-zone"],
+        "data": ["hand-zone", "player-one"]
+    },
+    {
+        "id": 26,
+        "classList": ["player-one-hand-zone"],
+        "data": ["hand-zone", "player-one"]
+    },
+    {
+        "id": 27,
+        "classList": ["player-one-hand-zone"],
+        "data": ["hand-zone", "player-one"]
+    },
+    {
+        "id": 28,
+        "classList": ["player-one-deck"]
+    }
+]
